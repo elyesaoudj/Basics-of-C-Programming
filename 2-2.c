00af8294f9fda9ae5e0644e32b23afaa13b52d9c
elyesaoudj <150586689+elyesaoudj@users.noreply.github.com>
2024-12-22 13:25:02 +0100

    Add files via upload

---
+++ 2-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("enter three numbers :");
+    scanf("%d",&a);
+    if (a>0)
+    {
+        printf("the number is positive");
+    }
+    else if (a<0) {
+        printf("the number is negative");
+    }
+    else{
+        printf("the number is zero");
+    }
+    return 0;
+}
+
