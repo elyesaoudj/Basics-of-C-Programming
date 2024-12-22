be40e6f53c03c1a74b9ae964f666d6063b4f582d
elyesaoudj <150586689+elyesaoudj@users.noreply.github.com>
2024-12-22 12:42:52 +0100

    Add files via upload

---
+++ 1-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int a,b;
+    printf("enter a:");
+    scanf("%d",&a);
+    printf("enter b:");
+    scanf("%d",&b);
+    printf("the sum is :%d",a+b);
+    return 0;
+}
