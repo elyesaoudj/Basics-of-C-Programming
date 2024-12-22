f42c4f40cc2f25285804fba55aea25fa464ac810
elyesaoudj <150586689+elyesaoudj@users.noreply.github.com>
2024-12-22 12:47:54 +0100

    Add files via upload

---
+++ 1-3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int length,breadth;
+    printf("enter length:");
+    scanf("%d",&length);
+    printf("enter breadth:");
+    scanf("%d",&breadth);
+    printf("the area of the rectangle is :%d ",length*breadth);
+    return 0;
+}
